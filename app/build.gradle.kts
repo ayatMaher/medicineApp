41a08a0f1b4f16fd611d271179d88ce9d77aa99a
Ansam Elkhalili <ansamalkhalili2001@gmail.com>
2025-05-27 11:19:55 +0300

    - create setting page layout - add rubik font - add primary and secondary colors - add icons as png *3 - make backgroud rectangle with corner radius

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.medicineapplication"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.medicineapplication"

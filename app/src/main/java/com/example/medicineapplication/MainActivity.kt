fc19007778fbee3d59a7795573629ffc6ffb9b63
~Ayat Maher <121240348+ayatMaher@users.noreply.github.com>
2025-05-29 16:25:33 +0300

    homeEdits

--- app/src/main/java/com/example/medicineapplication/MainActivity.kt
+++ app/src/main/java/com/example/medicineapplication/MainActivity.kt
@@ -7,15 +7,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.medicineapplication.adapter.MedicineTypeAdapter
+import com.example.medicineapplication.adapter.PharmacyHomeAdapter
 import com.example.medicineapplication.model.MedicineType
+import com.example.medicineapplication.model.Pharmacy
 
-public class MainActivity : AppCompatActivity(),MedicineTypeAdapter.ItemClickListener {
+public class MainActivity : AppCompatActivity(), MedicineTypeAdapter.ItemClickListener,
+    PharmacyHomeAdapter.ItemClickListener {
+    //medicine type
     lateinit var medicineTypeAdapter: MedicineTypeAdapter
     lateinit var rvMedicineType: RecyclerView
     var items: ArrayList<MedicineType> = ArrayList<MedicineType>()
-
+    //pharmacy
+    lateinit var pharmacyHomeAdapter: PharmacyHomeAdapter
+    lateinit var rvPharmacy: RecyclerView
+    var pharmacy_item: ArrayList<Pharmacy> = ArrayList<Pharmacy>()
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +37,7 @@ public class MainActivity : AppCompatActivity(),MedicineTypeAdapter.ItemClickLis
         }
 
         //Medicine type
-        rvMedicineType= findViewById(R.id.rvMedicinetype);
+        rvMedicineType = findViewById(R.id.rvMedicinetype);
         items.add(MedicineType("1", R.drawable.image1, "أقراص"))
         items.add(MedicineType("2", R.drawable.image2, "شراب"))
         items.add(MedicineType("3", R.drawable.image3, "حقن"))
@@ -43,12 +51,30 @@ public class MainActivity : AppCompatActivity(),MedicineTypeAdapter.ItemClickLis
         rvMedicineType.setLayoutManager(GridLayoutManager(this, 4)) // 4 columns
         rvMedicineType.setAdapter(medicineTypeAdapter)
 
+        // Pharmacy
+        rvPharmacy=findViewById(R.id.rvPharmacy)
+        pharmacy_item.add(Pharmacy("1",R.drawable.pharmacy_img,"صيدلية الخنساء الطبية",
+            "4.5","شارع بور سعيد "))
+        pharmacy_item.add(Pharmacy("2",R.drawable.pharmacy_img,"صيدلية الخنساء الطبية",
+            "4.5","شارع بور سعيد "))
+        pharmacy_item.add(Pharmacy("3",R.drawable.pharmacy_img,"صيدلية الخنساء الطبية",
+            "4.5","شارع بو سعيد "))
+        pharmacy_item.add(Pharmacy("4",R.drawable.pharmacy_img,"صيدلية الخنساء الطبية",
+            "4.5","شارع بور سعيد "))
+
+        pharmacyHomeAdapter=PharmacyHomeAdapter(this,pharmacy_item,this)
+        rvPharmacy.setAdapter(pharmacyHomeAdapter)
 
 
     }
 
     override fun onItemClick(position: Int, id: String?) {
         //when click to type card
+        TODO("Not yet implemented")
+    }
+
+    override fun onItemClickPharmacy(position: Int, id: String?) {
+        //when click to pharmacy card
         TODO("Not yet implemented")
     }
 }
---
+++ app/src/main/java/com/example/medicineapplication/model/Pharmacy.kt
@@ -0,0 +1,9 @@
+package com.example.medicineapplication.model
+
+data class Pharmacy(
+    val id: String,
+    val pharmacyImage: Int,
+    val pharmacyName: String,
+    val rate: String,
+    val pharmacyAddress: String
+)

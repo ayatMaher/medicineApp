b966cba1227b19c0336edfce83c279571aa94730
Ansam Elkhalili <ansamalkhalili2001@gmail.com>
2025-06-22 10:43:35 +0300

    create common Quation page activity create Quations adapter create quation item

---
+++ app/src/main/java/com/example/medicineapplication/CommonQuationsPage.kt
@@ -0,0 +1,49 @@
+package com.example.medicineapplication
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.medicineapplication.adapter.QuationsAdapter
+import com.example.medicineapplication.model.quationItem
+
+class CommonQuationsPage : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var faqAdapter: QuationsAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_common_quations_page)
+
+        recyclerView = findViewById(R.id.faqRecyclerView)
+
+        val faqList = listOf(
+            quationItem(
+                "كيف يمكنني معرفة العلاج المناسب لحالتي؟",
+                "بعد تعبئة بياناتك الصحية، سيقترح التطبيق العلاجات المناسبة بناءً على حالتك."
+            ),
+            quationItem(
+                "هل التطبيق مجاني؟",
+                "نعم، التطبيق مجاني للاستخدام الأساسي."
+            ),
+            quationItem(
+                "هل يمكنني التواصل مع طبيب؟",
+                "نعم، التطبيق يوفر خاصية التواصل مع مختصين."
+            )
+        )
+
+        faqAdapter = QuationsAdapter(faqList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = faqAdapter
+
+
+
+
+
+    }
+}

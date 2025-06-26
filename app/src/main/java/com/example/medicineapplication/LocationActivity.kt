2e5de69b3cd5c6074c6e3337b0950af5f128c842
Ansam Elkhalili <ansamalkhalili2001@gmail.com>
2025-06-26 18:49:37 +0300

    create location activity and fix some bugs

---
+++ app/src/main/java/com/example/medicineapplication/LocationActivity.kt
@@ -0,0 +1,28 @@
+package com.example.medicineapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.example.medicineapplication.databinding.ActivityForgetPasswordBinding
+import com.example.medicineapplication.databinding.ActivityLocationBinding
+
+class LocationActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLocationBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityLocationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //statusBar Color
+        window.statusBarColor = ContextCompat.getColor(this, R.color.status_bar_color_log)
+        // اجعل الأيقونات داكنة إذا كان الخلفية فاتحة
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = true
+
+
+    }
+}
